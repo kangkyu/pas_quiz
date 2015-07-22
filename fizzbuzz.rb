5cdfd1c33a95aac69c7ce21ff3bdaa784e0c7281
kangkyu <kangkyu1111@gmail.com>
2015-07-22 14:17:03 -0700

    refactor more, and array test, hash test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,26 +4,15 @@ class Fizzbuzz
     @limit = n
   end
 
-  # for testing
-  def result_array
-    result = []
-    (1..limit).each do |number|
-      fizzbuzz = ""
-      fizzbuzz += "fizz" if number % 3 == 0
-      fizzbuzz += "buzz" if number % 5 == 0
-      result << [number, fizzbuzz]
-    end
-    result
-  end
-
   # ruby fizzbuzz.rb
   def result_console
     (1..limit).each do |number|
-      puts number.to_s +
-        " " +
-        fizz_for(number) +
-        buzz_for(number)
+      puts number.to_s + " " + fizzbuzz_for(number)
     end
+  end
+
+  def fizzbuzz_for(number)
+    fizz_for(number) + buzz_for(number)
   end
 
   def fizz_for(number)
@@ -33,6 +22,38 @@ class Fizzbuzz
   def buzz_for(number)
     number % 5 == 0 ? "buzz" : ""
   end
+
+# for testing #
+
+  def result_hash
+    result = {}
+    (1..limit).each do |number|
+      result.merge!({number => fizzbuzz_for(number)})
+    end
+    result
+  end
+
+  def result_array
+    result = []
+    (1..limit).each do |number|
+      result << [number, fizzbuzz_for(number)]
+    end
+    result
+  end
+
+  # def result_console_from_hash
+  #   result_hash.each do |k, v|
+  #     puts "#{k} #{v}"
+  #   end
+  # end
+
+  # def result_console_from_array
+  #   result_array.each do |a, b|
+  #     puts "#{a} #{b}"
+  #   end
+  # end
 end
 
 Fizzbuzz.new(100).result_console
+# Fizzbuzz.new(100).result_console_from_hash
+# Fizzbuzz.new(100).result_console_from_array
